3837fe78eac68be24f7c698d42bf24d9856aa0a8
Abhijit Dash <atd5370@gmail.com>
2025-12-20 01:15:54 +0530

    Add new API endpoints for stats and attendees

--- scanner/urls.py
+++ scanner/urls.py
@@ -3,5 +3,7 @@ from .views import scan_qr, scanner_page
 
 urlpatterns = [
     path("", scanner_page),      # opens scanner
-    path("scan/", scan_qr),      # API endpoint
+    path("scan/", scan_qr),     # API endpoint
+    path('api/stats/', views.stats_view, name='stats'),
+path('api/attendees/', views.attendees_view, name='attendees'), 
 ]
